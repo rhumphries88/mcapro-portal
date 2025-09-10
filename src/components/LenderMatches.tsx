be61c14a6a6579b3a5454d8932245687e3d3cfae
rentalizer <rich@istayusa.com>
2025-09-11 02:07:47 +0800

    09112025v1

--- src/components/LenderMatches.tsx
+++ src/components/LenderMatches.tsx
@@ -147,7 +147,7 @@ const LenderMatches: React.FC<LenderMatchesProps> = ({ application, matches, onL
       </div>
     );
   }
-
+//james 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       {/* Header Section */}
